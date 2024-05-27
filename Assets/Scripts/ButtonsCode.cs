3b853042492471b4a745bfb29b137dd6c6a49aa4
MadMadMattMan <92753107+MadMadMattMan@users.noreply.github.com>
2024-05-28 10:25:57 +1200

    Dub

--- Assets/Scripts/ButtonsCode.cs
+++ Assets/Scripts/ButtonsCode.cs
@@ -10,5 +10,20 @@ public class ButtonsCode : MonoBehaviour
         SceneManager.LoadScene(1);
         Debug.Log("Clicked Bowling");
     }
+
+    public void LoadGolf()
+    {
+        SceneManager.LoadScene(2);
+        Debug.Log("Clicked Golf");
+    }
+    public void LoadClayShooting()
+    {
+        SceneManager.LoadScene(3);
+        Debug.Log("Clicked ClayShooting");
+    }
+    public void LoadQuit()
+    {
+       Application.Quit();
+    }
 }
     
