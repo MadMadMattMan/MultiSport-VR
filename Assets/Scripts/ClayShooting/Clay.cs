d18c9494a9e4ffd80cc612407b265b15379b0b15
NavDogChur <navik.alwis@hibs.school.nz>
2024-06-25 10:10:56 +1200

    twrete

--- Assets/Scripts/ClayShooting/Clay.cs
+++ Assets/Scripts/ClayShooting/Clay.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Clay : MonoBehaviour
 {
- 
+    public int score = 0;
+    [SerializeField] TextMeshProUGUI scoretext;
 
     // Update is called once per frame
+
+    void Start()
+    {
+        score = 0;
+    }
     void Update()
     {
        if(transform.position.y < 0)
@@ -22,7 +29,8 @@ public class Clay : MonoBehaviour
             Destroy(collision.gameObject); 
             // Particle system
             Destroy(gameObject);
-
+            score = score+1;
+            scoretext.text = "Score: " + score.ToString();
 
         }
     }
